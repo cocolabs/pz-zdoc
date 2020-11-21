b0c6e5d55c7f033ce987ac0a75ee11b30ea9ed38
Matthew Cain <yooksi@zoho.com>
2020-11-21 11:59:05 +0100

    Add main java files

---
+++ tools/src/main/java/io/yooksi/pzlua/tools/Logger.java
@@ -0,0 +1,31 @@
+package io.yooksi.pzlua.tools;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
+
+public class Logger {
+
+	private final File LOG_FILE;
+
+	public Logger() {
+		try {
+			File logDir = new File("logs");
+			if (!logDir.exists() && !logDir.mkdir()) {
+				throw new IOException("Unable to create log directory.");
+			}
+			LOG_FILE = Paths.get(logDir.getPath(), "main.log").toFile();
+			FileUtils.write(LOG_FILE, "", Charset.defaultCharset(), false);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void print(String log) throws IOException {
+		FileUtils.write(LOG_FILE, log, Charset.defaultCharset(), true);
+	}
+}
