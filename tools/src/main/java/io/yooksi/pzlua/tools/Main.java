1855a5ab8cd644a4010d15a82f137cc03213f775
Matthew Cain <yooksi@zoho.com>
2020-11-23 19:25:47 +0100

    Rename application argument

--- tools/src/main/java/io/yooksi/pzlua/tools/Main.java
+++ tools/src/main/java/io/yooksi/pzlua/tools/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
         for (int i = 0; i < args.length; i++)
         {
-            if (args[i].equals("--document-lua"))
+            if (args[i].equals("-lua"))
             {
                 Path path; java.util.List<Path> paths;
                 try {
