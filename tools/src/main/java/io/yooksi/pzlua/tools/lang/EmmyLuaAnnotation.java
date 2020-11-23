66b69b4be936cbb44dce45b20777faacf93cc438
Matthew Cain <yooksi@zoho.com>
2020-11-23 17:54:11 +0100

    Add EmmyLua annotation class

---
+++ tools/src/main/java/io/yooksi/pzlua/tools/lang/EmmyLuaAnnotation.java
@@ -0,0 +1,30 @@
+package io.yooksi.pzlua.tools.lang;
+
+import java.util.regex.Pattern;
+
+public enum EmmyLuaAnnotation {
+
+	CLASS("class") {
+		@Override
+		public String create(String name, String...params) {
+			return getBase() + name;
+		}
+	};
+
+	private final String keyword;
+	EmmyLuaAnnotation(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public String getBase() {
+		return "---@" + keyword;
+	}
+
+	public boolean isAnnotation(String text) {
+
+		Pattern regex = Pattern.compile(getBase());
+		return regex.matcher(text).find();
+	}
+
+	public abstract String create(String name, String...params);
+}
