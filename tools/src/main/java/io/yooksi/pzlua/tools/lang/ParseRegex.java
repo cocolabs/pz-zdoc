23550721e4a77d6f33e1a6913cca9b204f8fb120
Matthew Cain <yooksi@zoho.com>
2020-11-24 12:31:38 +0100

    Create ParseRegex class

---
+++ tools/src/main/java/io/yooksi/pzlua/tools/lang/ParseRegex.java
@@ -0,0 +1,21 @@
+package io.yooksi.pzlua.tools.lang;
+
+import java.util.regex.Pattern;
+
+public class ParseRegex {
+
+	/** Regex pattern used to parse java method from text */
+	public static final Pattern JAVA_METHOD_REGEX = Pattern.compile("^((?:final|static|abstract|" +
+			"transient|synchronized|volatile)\\s+)?([^\\s]+)\\s+([^\\s]+)\\((.+)?\\)$");
+
+	/**
+	 * @param matcher process that contains regex groups
+	 * @param i index of group to retrieve
+	 * @return the matched group <i>or</i> an <b>empty</b> string
+	 */
+	public static String getMatchedGroup(java.util.regex.Matcher matcher, int i) {
+
+		String result = matcher.group(i);
+		return result != null ? result : "";
+	}
+}
