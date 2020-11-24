a01b54f67ef39d45d9bc5dc2d4918f785f769899
Matthew Cain <yooksi@zoho.com>
2020-11-24 15:57:49 +0100

    Fix app not reading arguments

--- src/main/java/io/yooksi/pzlua/tools/Main.java
+++ src/main/java/io/yooksi/pzlua/tools/Main.java
@@ -25,56 +25,55 @@ public class Main {
     		throw new IllegalArgumentException("No application argument supplied");
 		}
 		Pattern argRegex = Pattern.compile("\\s*+-(\\w+)\\s*");
-        for (int i = 0; i < args.length; i++)
-        {
-        	String rawOpArg = args[i];
-        	Matcher matcher = argRegex.matcher(rawOpArg);
 
-        	if (!matcher.find()) {
-				throw new IllegalArgumentException("Malformed application argument: " + rawOpArg);
+		String rawOpArg = args[0];
+		Matcher matcher = argRegex.matcher(rawOpArg);
+
+		if (!matcher.find()) {
+			throw new IllegalArgumentException("Malformed application argument: " + rawOpArg);
+		}
+		String opArg = matcher.group(1);
+		if (opArg.equals("lua"))
+		{
+			Path path; java.util.List<Path> paths;
+			try {
+				path = Paths.get(args[1]);
+				paths = Files.walk(Paths.get(path.toString())).filter(Files::isRegularFile)
+						.collect(Collectors.toCollection(ArrayList::new));
 			}
-        	String opArg = matcher.group(1);
-        	if (opArg.equals("-lua"))
-            {
-                Path path; java.util.List<Path> paths;
-                try {
-                    path = Paths.get(args[i + 1]);
-                    paths = Files.walk(Paths.get(path.toString())).filter(Files::isRegularFile)
-                            .collect(Collectors.toCollection(ArrayList::new));
-                }
-                catch(InvalidPathException e) {
-                    throw new RuntimeException("Invalid file path: " + args[i + 1], e);
-                }
-                catch(IndexOutOfBoundsException e) {
-                    throw new RuntimeException("No file path supplied", e);
-                }
-                int docsWritten = 0;
-                for (Path docPath : paths) {
-                    if (LuaParser.documentLuaFile(docPath.toFile())) {
-                        docsWritten += 1;
-                    }
-                }
-                System.out.printf("Documented %d classes in %s%n", docsWritten, path.toString());
-            }
-            // document java to lua
-            else if (opArg.equals("-java"))
-            {
-                Path path;
-                try {
-                    path = Paths.get(args[i + 1]);
-                }
-                catch(InvalidPathException e) {
-                    throw new RuntimeException("Invalid output file path: " + args[i + 1], e);
-                }
-                catch(IndexOutOfBoundsException e) {
-                    throw new RuntimeException("No output file path supplied", e);
-                }
-                String sUrl = args.length < i + 2 ? args[i + 2] : JavaDocParser.PZ_API_GLOBAL_URL;
-                URL url = new URL(JavaDocParser.PZ_API_URL.concat(sUrl));
+			catch(InvalidPathException e) {
+				throw new RuntimeException("Invalid file path: " + args[1], e);
+			}
+			catch(IndexOutOfBoundsException e) {
+				throw new RuntimeException("No file path supplied", e);
+			}
+			int docsWritten = 0;
+			for (Path docPath : paths) {
+				if (LuaParser.documentLuaFile(docPath.toFile())) {
+					System.out.println("- Documented lua file " + docPath.relativize(PROJECT_DIR));
+					docsWritten += 1;
+				}
+			}
+			System.out.printf("Documented %d classes in %s%n", docsWritten, path.toString());
+		}
+		// document java to lua
+		else if (opArg.equals("java"))
+		{
+			Path path;
+			try {
+				path = Paths.get(args[1]);
+			}
+			catch(InvalidPathException e) {
+				throw new RuntimeException("Invalid output file path: " + args[1], e);
+			}
+			catch(IndexOutOfBoundsException e) {
+				throw new RuntimeException("No output file path supplied", e);
+			}
+			String sUrl = args.length >= 3 ? args[2] : JavaDocParser.PZ_API_GLOBAL_URL;
+			URL url = new URL(JavaDocParser.PZ_API_URL.concat(sUrl));
 
-				JavaDocParser.create(url.toString()).convertJavaToLuaDoc(path);
-            }
-            else throw new IllegalArgumentException("Unknown application argument: " + opArg);
-        }
+			JavaDocParser.create(url.toString()).convertJavaToLuaDoc(path);
+		}
+		else throw new IllegalArgumentException("Unknown application argument: " + opArg);
     }
 }
