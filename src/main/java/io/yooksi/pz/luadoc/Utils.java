02cc81754a3ffe38918562bcda12f4f85c67b4f7
Matthew Cain <yooksi@zoho.com>
2020-11-25 16:29:53 +0100

    Fix app processing non-lua files

--- src/main/java/io/yooksi/pz/luadoc/Utils.java
+++ src/main/java/io/yooksi/pz/luadoc/Utils.java
@@ -1,5 +1,7 @@
 package io.yooksi.pz.luadoc;
 
+import org.apache.commons.io.FilenameUtils;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.InvalidPathException;
@@ -7,6 +9,22 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Utils {
+
+	/**
+	 * @return {@code true} if file denoted by given filename is a lua file
+	 */
+	public static boolean isLuaFile(String filename) {
+		return FilenameUtils.getExtension(filename).equals("lua");
+	}
+
+	/**
+	 * @return {@code true} if file denoted by given path is a lua file
+	 * @throws NullPointerException if given path has no elements
+	 */
+	public static boolean isLuaFile(Path path) {
+		return isLuaFile(path.getFileName().toString());
+	}
+
 	public static boolean isValidUrl(String url) {
 
 		try { new URL(url); }
