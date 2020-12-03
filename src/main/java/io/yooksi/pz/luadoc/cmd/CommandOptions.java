462283d4813ba000c4578da91bda21b48cf70671
Matthew Cain <yooksi@zoho.com>
2020-12-03 09:15:37 +0100

    Add command arg display names

--- src/main/java/io/yooksi/pz/luadoc/cmd/CommandOptions.java
+++ src/main/java/io/yooksi/pz/luadoc/cmd/CommandOptions.java
@@ -11,15 +11,18 @@ public final class CommandOptions {
 
 	static final Option INPUT_OPTION =
 			Option.builder("i").longOpt("input-path").desc("input directory path")
-					.type(File.class).required(true).hasArg().valueSeparator(' ').build();
+					.type(File.class).required(true).hasArg().argName("path")
+					.valueSeparator(' ').build();
 
 	static final Option OUTPUT_OPTION =
 			Option.builder("o").longOpt("output-path").desc("output directory path")
-					.type(File.class).required(false).hasArg().valueSeparator(' ').build();
+					.type(File.class).required(false).hasArg().argName("path")
+					.valueSeparator(' ').build();
 
 	static final Option API_OPTION =
-			Option.builder("a").longOpt("api-docs").desc("use online api docs")
-					.type(URL.class).required(false).hasArg().valueSeparator(' ').build();
+			Option.builder("a").longOpt("api-docs").desc("read online api from url")
+					.type(URL.class).required(false).hasArg().argName("url")
+					.valueSeparator(' ').build();
 
 	static final Options LUA_OPTIONS = new Options();
 	static final Options JAVA_OPTIONS = new Options();
